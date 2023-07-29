698fed03c8d677c631a4318ee058dc43d054ba33
toshiki goto <gotoshi.euphonium@gmail.com>
2023-07-30 00:33:46 +0900

    cannon apiがむずすぎ

--- cannon.c
+++ cannon.c
@@ -4,7 +4,7 @@
 
 #include <mpi.h>
 
-#define N 1152
+#define N 2400
 #define DEBUG 1
 #define EPS 1.0e-18
 
@@ -14,65 +14,88 @@ static double C[N][N];
 
 int myid, numprocs;
 
-void get_topology_info(int root_numprocs, int *left, int *right, int *up,
-                       int *down, MPI_Comm *comm) {
+void get_topology_info(int root_numprocs, MPI_Comm *comm) {
   int dim[2] = {root_numprocs, root_numprocs};
   int periods[2] = {1, 1};
   MPI_Cart_create(MPI_COMM_WORLD, 2, dim, periods, 1, comm);
-  MPI_Cart_shift(*comm, 0, 1, left, right);
-  MPI_Cart_shift(*comm, 1, 1, up, down);
 }
 
 void calculate_local_matmal(double *A, double *B, double *C, int Nl) {
   int i, j, k;
-  for (i = 0; i < Nl; i++)
-    for (k = 0; k < Nl; k++)
-      for (j = 0; j < Nl; j++)
-        C[i * Nl + j] += A[i * Nl + k] * B[k * Nl + j];
+  int tmpa;
+  for (i = 0; i < Nl; i++) {
+    for (k = 0; k < Nl; k++) {
+      tmpa = A[i * Nl + k];
+      for (j = 0; j < Nl; j++) {
+        C[i * Nl + j] += tmpa * B[k * Nl + j];
+      }
+    }
+  }
 }
 
-void solve(double *A, double *B, double *C, int Nl, int root_numproc, int left,
-           int right, int up, int down, MPI_Comm cannon_comm) {
+void solve(double A[N][N], double B[N][N], double C[N][N], int Nl,
+           int root_numproc, MPI_Comm cannon_comm) {
 
-  double *ALocal = (double *)malloc(Nl * Nl * sizeof(double));
-  double *BLocal = (double *)malloc(Nl * Nl * sizeof(double));
-  double *CLocal = (double *)malloc(Nl * Nl * sizeof(double));
+  int i, j;
 
-  MPI_Scatter(A, Nl * Nl, MPI_DOUBLE, ALocal, Nl * Nl, MPI_DOUBLE, 0,
-              cannon_comm);
-  MPI_Scatter(B, Nl * Nl, MPI_DOUBLE, BLocal, Nl * Nl, MPI_DOUBLE, 0,
-              cannon_comm);
+  double *localA = (double *)malloc(Nl * Nl * sizeof(double));
+  double *localB = (double *)malloc(Nl * Nl * sizeof(double));
+  double *localC = (double *)calloc(Nl * Nl, sizeof(double));
+
+  MPI_Datatype my_mat_type, type;
+  MPI_Type_vector(Nl, Nl, N, MPI_DOUBLE, &type);
+  MPI_Type_create_resized(type, 0, Nl * sizeof(double), &my_mat_type);
+  MPI_Type_commit(&my_mat_type);
+
+  int *sendcounts = (int *)malloc(numprocs * sizeof(int));
+  int *displs = (int *)malloc(numprocs * sizeof(int));
+
+  int disp = 0;
+  for (i = 0; i < root_numproc; i++) {
+    for (j = 0; j < root_numproc; j++) {
+      sendcounts[i * root_numproc + j] = 1;
+      displs[i * root_numproc + j] = disp;
+      disp += 1;
+    }
+    disp += (Nl - 1) * root_numproc;
+  }
+
+  MPI_Scatterv(&A[0][0], sendcounts, displs, my_mat_type, localA, Nl * Nl,
+               MPI_DOUBLE, 0, MPI_COMM_WORLD);
+  MPI_Scatterv(&B[0][0], sendcounts, displs, my_mat_type, localB, Nl * Nl,
+               MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+  // skewing
+  int coord[2];
+  int left, right, up, down;
+  MPI_Cart_coords(cannon_comm, myid, 2, coord);
+  MPI_Cart_shift(cannon_comm, 1, coord[0], &left, &right);
+  MPI_Sendrecv_replace(localA, Nl * Nl, MPI_DOUBLE, left, 1, right, 1,
+                       cannon_comm, MPI_STATUS_IGNORE);
+  MPI_Cart_shift(cannon_comm, 0, coord[1], &up, &down);
+  MPI_Sendrecv_replace(localB, Nl * Nl, MPI_DOUBLE, up, 1, down, 1, cannon_comm,
+                       MPI_STATUS_IGNORE);
 
   int num_shift;
-  double *buf = (double *)malloc(Nl * Nl * sizeof(double));
-  double *tmp;
 
+  MPI_Cart_shift(cannon_comm, 1, 1, &left, &right);
+  MPI_Cart_shift(cannon_comm, 0, 1, &up, &down);
   for (num_shift = 0; num_shift < root_numproc - 1; num_shift++) {
     // Matrix multiplication
-    calculate_local_matmal(ALocal, BLocal, CLocal, Nl);
-    // Communication
-    MPI_Sendrecv_replace(ALocal, Nl * Nl, MPI_DOUBLE, left, 1, right, 1,
-                         cannon_comm, MPI_STATUS_IGNORE);
-    // MPI_Sendrecv(ALocal, Nl * Nl, MPI_DOUBLE, left, 1, buf, Nl * Nl,
-    // MPI_DOUBLE,
-    //              right, 1, cannon_comm, MPI_STATUS_IGNORE);
-    // tmp = buf;
-    // buf = ALocal;
-    // ALocal = tmp;
-    MPI_Sendrecv_replace(BLocal, Nl * Nl, MPI_DOUBLE, up, 2, down, 2,
-                         cannon_comm, MPI_STATUS_IGNORE);
-    // MPI_Sendrecv(BLocal, Nl * Nl, MPI_DOUBLE, up, 2, buf, Nl * Nl,
-    // MPI_DOUBLE,
-    //              down, 2, cannon_comm, MPI_STATUS_IGNORE);
-    // tmp = buf;
-    // buf = BLocal;
-    // BLocal = tmp;
-  }
-  calculate_local_matmal(ALocal, BLocal, CLocal, Nl);
+    calculate_local_matmal(localA, localB, localC, Nl);
 
-  MPI_Gather(CLocal, Nl * Nl, MPI_DOUBLE, C, Nl * Nl, MPI_DOUBLE, 0, cannon_comm);
+    MPI_Sendrecv_replace(localA, Nl * Nl, MPI_DOUBLE, left, 1, right, 1,
+                         cannon_comm, MPI_STATUS_IGNORE);
+    MPI_Sendrecv_replace(localB, Nl * Nl, MPI_DOUBLE, up, 2, down, 2,
+                         cannon_comm, MPI_STATUS_IGNORE);
+  }
+  calculate_local_matmal(localA, localB, localC, Nl);
+
+  MPI_Gatherv(localC, Nl * Nl, MPI_DOUBLE, &C[0][0], sendcounts, displs,
+              my_mat_type, 0, MPI_COMM_WORLD);
   return;
 }
+
 void initialize_matrix(double A[N][N], int n) {
   int i, j;
   double dc_inv = 1.0 / (double)RAND_MAX;
@@ -95,9 +118,8 @@ int main(int argc, char *argv[]) {
   ierr = MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
 
   int root_numprocs = sqrt(numprocs);
-  int left, right, up, down;
   MPI_Comm cannon_comm;
-  get_topology_info(root_numprocs, &left, &right, &up, &down, &cannon_comm);
+  get_topology_info(root_numprocs, &cannon_comm);
   int Nl = N / root_numprocs;
 
   if (myid == 0) {
@@ -106,20 +128,21 @@ int main(int argc, char *argv[]) {
       dc_inv = 1.0 / (double)RAND_MAX;
       for (i = 0; i < N; i++) {
         for (j = 0; j < N; j++) {
-          A[i][j] = rand() * dc_inv;
-          B[i][j] = A[i][j];
+          A[i][j] = 1.0;
+          B[i][i] = 1.0;
         }
       }
     } else {
       srand(1);
       initialize_matrix(A, N);
+      initialize_matrix(B, N);
     }
   }
 
   ierr = MPI_Barrier(cannon_comm);
   t1 = MPI_Wtime();
 
-  solve(A, B, C, Nl, root_numprocs, left, right, up, down, cannon_comm);
+  solve(A, B, C, Nl, root_numprocs, cannon_comm);
 
   t2 = MPI_Wtime();
   t0 = t2 - t1;
@@ -132,6 +155,18 @@ int main(int argc, char *argv[]) {
     double d_mflops = 2.0 * (double)N * (double)N * (double)N / t_w;
     d_mflops = d_mflops * 1.0e-6;
     printf(" %lf [MFLOPS] \n", d_mflops);
+    if (DEBUG) {
+      for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+          if (C[i][j] != 1) {
+            printf("Error! C[%d][%d]=%lf\n", i, j, C[i][j]);
+            rc = MPI_Finalize();
+            exit(1);
+          }
+        }
+      }
+      printf("OK!\n");
+    }
   }
 
   rc = MPI_Finalize();
---
+++ powm1.c
@@ -0,0 +1,191 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <mpi.h>
+
+#define DEBUG 0
+#define N 576 * 2
+#define MAX_ITER 100
+#define EPS 1e-16
+
+/* Please define the matrices in HERE. */
+static double A[N][N];
+static double x[N];
+static double y[N];
+
+int myid, numprocs;
+
+void MyMatVec(double y[N], double A[N][N], double x[N], int n, int n_local);
+double PowM(int n, int n_local, int *n_iter);
+
+void print_matrix(double *A_local, int n, int n_local) {
+  int i, j;
+  for (i = 0; i < n_local; i++) {
+    for (j = 0; j < n; j++) {
+      printf("%lf ", A_local[i * n + j]);
+    }
+    printf("\n");
+  }
+}
+
+int main(int argc, char *argv[]) {
+
+  double t0, t1, t2, t_w;
+  double dc_inv;
+  double dlambda;
+  double d_residual;
+  double d_tmp;
+
+  int ierr;
+  int i, j;
+  int n_iter;
+
+  ierr = MPI_Init(&argc, &argv);
+  ierr = MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+  ierr = MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+
+  srand(1);
+  dc_inv = 1.0 / (double)RAND_MAX;
+
+  for (j = 0; j < N; j++) {
+    x[j] = rand() * dc_inv;
+  }
+  for (j = 0; j < N; j++) {
+    for (i = j; i < N; i++) {
+      A[j][i] = rand() * dc_inv;
+      A[i][j] = A[j][i];
+    }
+  }
+
+  // printf("A matrix\n");
+  // print_matrix(&A[0][0], N, N);
+
+  /* Start of PowM routine ----------------------------*/
+  ierr = MPI_Barrier(MPI_COMM_WORLD);
+  t1 = MPI_Wtime();
+
+  dlambda = PowM(N, N / numprocs, &n_iter);
+
+  //     ierr = MPI_Barrier(MPI_COMM_WORLD);
+  t2 = MPI_Wtime();
+  t0 = t2 - t1;
+  ierr = MPI_Reduce(&t0, &t_w, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+  /* End of PowM routine ---------------------------- */
+
+  /* Calculate residual */
+
+  d_residual = 0.0;
+  for (i = 0; i < N; i++) {
+    d_tmp = 0.0;
+    for (j = 0; j < N; j++) {
+      d_tmp += A[i][j] * x[j];
+    }
+    d_residual += (d_tmp - dlambda * x[i]) * (d_tmp - dlambda * x[i]);
+  }
+  d_residual = sqrt(d_residual);
+
+  if (myid == 0) {
+    printf("N  = %d \n", N);
+    printf("Power Method time  = %lf [sec.] \n", t_w);
+
+    printf("Eigenvalue  = %e \n", dlambda);
+    if (n_iter == -1) {
+      printf("Not converged. \n");
+    } else {
+      printf("Iteration Number: %d \n", n_iter);
+    }
+    printf("Residual 2-Norm ||A x - lambda x||_2  = %e \n", d_residual);
+  }
+
+  ierr = MPI_Finalize();
+
+  exit(0);
+}
+
+void MyMatVec(double y[N], double A[N][N], double x[N], int n, int n_local) {
+  int i, j;
+  int start = myid * n_local;
+  int end = start + n_local;
+
+  for (i = start; i < end; i++) {
+    y[i] = 0.0;
+    for (j = 0; j < n; j++) {
+      y[i] += A[i][j] * x[j];
+    }
+  }
+}
+
+double PowM(int n, int n_local, int *n_iter) {
+  double d_tmp1, d_tmp2;
+  double dlambda;
+  double d_before = 0.0;
+
+  int i;
+  int i_loop;
+
+  int start = myid * n_local;
+  int end = start + n_local;
+
+  /* Normizeation of x */
+  d_tmp1 = 0.0;
+  for (i = start; i < end; i++) {
+    d_tmp1 += x[i] * x[i];
+  }
+  MPI_Allreduce(&d_tmp1, &d_tmp1, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+  d_tmp1 = 1.0 / sqrt(d_tmp1);
+  for (i = start; i < end; i++) {
+    x[i] *= d_tmp1;
+  }
+
+  /* Main iteration loop ---------------------- */
+  double *xt = (double *)malloc(sizeof(double) * n_local);
+  for (i_loop = 1; i_loop <= MAX_ITER; i_loop++) {
+    if (DEBUG && myid == 0)
+      printf("d_before = %e, dlambda = %e, EPS = %e \n", d_before, dlambda,
+             EPS);
+
+    // for (i = start; i < end; i++) {
+    //   xt[i - start] = x[i];
+    // }
+    /* Matrix Vector Product */
+    MPI_Allgather(x + start, n_local, MPI_DOUBLE, x, n_local, MPI_DOUBLE,
+                  MPI_COMM_WORLD);
+    if (DEBUG && myid == 0)
+      printf("x[0] = %e, x[1] = %e, x[2] = %e \n", x[0], x[1], x[2]);
+
+    MyMatVec(y, A, x, n, n_local);
+
+    /* innner products */
+    d_tmp1 = 0.0;
+    d_tmp2 = 0.0;
+    for (i = start; i < end; i++) {
+      d_tmp1 += y[i] * y[i];
+      d_tmp2 += y[i] * x[i];
+    }
+
+    MPI_Allreduce(&d_tmp1, &d_tmp1, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+    MPI_Allreduce(&d_tmp2, &d_tmp2, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+
+    /* current approximately eigenvalue */
+    dlambda = d_tmp1 / d_tmp2;
+
+    /* Convergence test*/
+    if (fabs(d_before - dlambda) < EPS) {
+      *n_iter = i_loop;
+      return dlambda;
+    }
+
+    /* keep current value */
+    d_before = dlambda;
+
+    /* Normalization and set new x */
+    d_tmp1 = 1.0 / sqrt(d_tmp1);
+    for (i = start; i < end; i++)
+      x[i] = y[i] * d_tmp1;
+
+  } /* end of i_loop -------------------------- */
+
+  *n_iter = -1;
+  return dlambda;
+}
